e37e133ac7eba130dc100049f82745d7ac03c9df
matteogara <m.garagiola@hotmail.com>
2020-12-21 11:46:41 +0100

    Text visibility

--- Assets/02_SCRIPTS/ScriptableObjects/ViewScriptableObject.cs
+++ Assets/02_SCRIPTS/ScriptableObjects/ViewScriptableObject.cs
@@ -3,7 +3,7 @@
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ViewScriptableObject", order = 1)]
 public class ViewScriptableObject : ScriptableObject
 {
-    public string targetName;
+    public string viewName;
     public Vector3 rotation;
     public float distance;
 }
--- Assets/02_SCRIPTS/ViewManager.cs
+++ Assets/02_SCRIPTS/ViewManager.cs
@@ -4,34 +4,60 @@ using UnityEngine;
 
 public class ViewManager : MonoBehaviour
 {
-    public ViewScriptableObject view1;
-    public ViewScriptableObject view2;
     public MouseOrbit mouseOrbit;
 
-    bool one = true;
+    public ViewScriptableObject[] viewData;
+    public GameObject[] viewTexts;
+
+
+
 
     void Start()
     {
-        mouseOrbit.target = GameObject.Find(view1.targetName).transform;
-        mouseOrbit.newRotation = Quaternion.Euler(view1.rotation);
-        mouseOrbit.newDistance = view1.distance;
-        one = true;
+        ChangeView("View1");
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            if (one) {
-                mouseOrbit.target = GameObject.Find(view2.targetName).transform;
-                mouseOrbit.newRotation = Quaternion.Euler(view2.rotation);
-                mouseOrbit.newDistance = view2.distance;
-                one = false;
-            } else {
-                mouseOrbit.target = GameObject.Find(view1.targetName).transform;
-                mouseOrbit.newRotation = Quaternion.Euler(view1.rotation);
-                mouseOrbit.newDistance = view1.distance;
-                one = true;
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                ChangeView(hit.collider.tag);
+                Debug.Log(hit.collider.tag);
             }
         }
+    }
+
+
+    void ChangeView(string _tag)
+    {
+        int _index = -1;
+        for (int i = 0; i < viewData.Length; i++)
+        {
+            if (_tag == viewData[i].viewName) _index = i;
+        }
+        if (_index < 0) return;
+
+        mouseOrbit.target = GameObject.Find(viewData[_index].viewName).transform;
+        mouseOrbit.newRotation = Quaternion.Euler(viewData[_index].rotation);
+        mouseOrbit.newDistance = viewData[_index].distance;
+
+        UpdateText(_index);
+    }
+
+
+    void UpdateText(int _index)
+    {
+        for (int i = 0; i < viewTexts.Length; i++)
+        {
+            viewTexts[i].SetActive(false);
+        }
+
+        viewTexts[_index].SetActive(true);
     }
 }
